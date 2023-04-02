afff18302e5ec8ecf5c2aa216d90773e3d54c991
bimogempar <bimogempar@gmail.com>
2023-04-02 16:49:11 +0700

    update: change env firebase config using console mobile-jajanku

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAWzj-UzQoP8DrkTmVXzFO_t0NN2xuQxmw",
-    authDomain: "react-native-firebase-au-99921.firebaseapp.com",
-    projectId: "react-native-firebase-au-99921",
-    storageBucket: "react-native-firebase-au-99921.appspot.com",
-    messagingSenderId: "571917929531",
-    appId: "1:571917929531:web:4f99441a4d560cf160201e"
+    apiKey: "AIzaSyBs8VScUzRPeFVe-MNP_BZ2csQyEPJSAbI",
+    authDomain: "jajanku-cb527.firebaseapp.com",
+    projectId: "jajanku-cb527",
+    storageBucket: "jajanku-cb527.appspot.com",
+    messagingSenderId: "676616856186",
+    appId: "1:676616856186:web:0f57dc15134f285b6bd1c3"
 }
 
 const app = initializeApp(firebaseConfig);
